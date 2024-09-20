fcefc45f6fff4e6fe5963ab828a1113f31897923
HarshilD05 <hdamania31@gmail.com>
2024-09-20 11:20:20 +0530

    Education Videos

--- src/components/Education/index.jsx
+++ src/components/Education/index.jsx
@@ -3,28 +3,24 @@ import { FocusCards } from "../ui/focus-cards";
 export default function Education() {
   const cards = [
     {
-      title: "Forest Adventure",
-      src: "https://images.unsplash.com/photo-1518710843675-2540dd79065c?q=80&w=3387&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      title: "Make a Living on a Smal Farm! (7 Step Tutorial)",
+      src: "http://i3.ytimg.com/vi/fRlUhUWS0Hk/hqdefault.jpg",
+      link: "https://www.youtube.com/watch?app=desktop&v=fRlUhUWS0Hk"
     },
     {
-      title: "Valley of life",
-      src: "https://images.unsplash.com/photo-1600271772470-bd22a42787b3?q=80&w=3072&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      title: "How is Singapore Leading the way in Efficient Urban Farming?",
+      src: "http://i3.ytimg.com/vi/W9tGyNyfDbs/hqdefault.jpg",
+      link: "https://www.youtube.com/watch?v=W9tGyNyfDbs"
     },
     {
-      title: "Sala behta hi jayega",
-      src: "https://images.unsplash.com/photo-1505142468610-359e7d316be0?q=80&w=3070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      title: "Top 5 Benefits of Urban Farming Explainer Video",
+      src: "http://i3.ytimg.com/vi/2h59wJjQfiE/hqdefault.jpg",
+      link: "https://www.youtube.com/watch?v=2h59wJjQfiE"
     },
     {
-      title: "Camping is for pros",
-      src: "https://images.unsplash.com/photo-1486915309851-b0cc1f8a0084?q=80&w=3387&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-    },
-    {
-      title: "The road not taken",
-      src: "https://images.unsplash.com/photo-1507041957456-9c397ce39c97?q=80&w=3456&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-    },
-    {
-      title: "The First Rule",
-      src: "https://assets.aceternity.com/the-first-rule.png",
+      title: "Urban Farming - Urban Agriculture",
+      src: "http://i3.ytimg.com/vi/ckeXvykKe9A/hqdefault.jpg",
+      link: "https://www.youtube.com/watch?v=ckeXvykKe9A"
     },
   ];
 
--- src/components/ui/focus-cards.jsx
+++ src/components/ui/focus-cards.jsx
@@ -11,7 +11,7 @@ export const Card = React.memo(({
     onMouseEnter={() => setHovered(index)}
     onMouseLeave={() => setHovered(null)}
     className={cn(
-      "rounded-lg relative bg-gray-100 dark:bg-neutral-900 overflow-hidden h-60 md:h-96 w-full transition-all duration-300 ease-out",
+      "rounded-lg relative bg-gray-100 dark:bg-neutral-900 overflow-hidden h-20 md:h-56 w-full transition-all duration-300 ease-out",
       hovered !== null && hovered !== index && "blur-sm scale-[0.98]"
     )}>
     <img
